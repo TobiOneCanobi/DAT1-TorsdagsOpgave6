b533437f59200e5e3bfe9e91a1fa6236837335d0
TobiOneCanobi <vlogkongen@gmail.com>
2023-10-12 09:45:44 +0200

    first push

---
+++ DAT1-TorsdagsOpgave6/src/Task1/Main.java
@@ -0,0 +1,15 @@
+package Task1;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Methods methods = new Methods();
+
+        methods.methodA();
+        methods.methodB();
+        methods.methodC();
+        methods.methodD();
+
+    }
+}
---
+++ DAT1-TorsdagsOpgave6/src/Task1/Methods.java
@@ -0,0 +1,24 @@
+package Task1;
+
+public class Methods
+{
+    public void methodA()
+    {
+        System.out.println("a");
+    }
+
+    public void methodB()
+    {
+        System.out.println("b");
+    }
+
+    public void methodC()
+    {
+        System.out.println("c");
+    }
+
+    public void methodD()
+    {
+        System.out.println("d");
+    }
+}
